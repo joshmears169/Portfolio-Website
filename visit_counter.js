50004a995332e4a277e0d9b0ce0b89d4bffd1716
Josh Mears <joshmears169@hotmail.com>
2020-10-15 20:30:35 +0100

    js edit

--- visit_counter.js
+++ visit_counter.js
@@ -3,7 +3,7 @@ const countEl = document.getElementById('count');
 updateVisitCount();
 
 function updateVisitCount() {
-    fetch('https://db0u1sqe9d.execute-api.us-east-2.amazonaws.com/prod/cou')
+    fetch('httpdkclmsd')
         .then(res => res.json())
         .then(res => {
         countEl.innerHTML = res.visits;
