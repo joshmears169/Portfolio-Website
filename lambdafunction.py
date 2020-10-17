2841d84a3aaf589d1f81dd485000c2a972e63d4c
Josh Mears <joshmears169@hotmail.com>
2020-10-17 16:59:06 +0100

    lambda work in progress

--- lambdafunction.py
+++ lambdafunction.py
@@ -1,24 +1,32 @@
 import boto3
 import json
-
-#def lambda_handler(event, context):
    
 dynamodb = boto3.resource('dynamodb')
    
 table = dynamodb.Table('Visitors')
-   
-response = table.get_item(
-    Key={
-        'website': 'joshmearsportfolio'
-     }
-   )
-count = response['Item']
-print(count)
 
-def update_count(): 
-    count['visitors'] += 1
-    return count
+def lambda_handler(event, context):  
 
-print(update_count())
+    response = table.get_item(
+        Key={
+            'website': 'joshmearsportfolio'
+        }
+    )
+    count = response['Item']
 
-table.put_item(Item=count)
+    def update_count(): 
+        count['visitors'] += 1
+        return count
+
+    table.put_item(Item=count)
+
+    response = table.get_item(
+        Key={
+            'website': 'joshmearsportfolio'
+        }
+    )
+    count = response['Item']
+
+    return {
+           'body': count
+        }
