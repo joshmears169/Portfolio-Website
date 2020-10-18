2525b127e3371aa96ee20a63fedeb777157a541c
Josh Mears <joshmears169@hotmail.com>
2020-10-18 16:12:26 +0100

    Test4

--- visit_counter.js
+++ visit_counter.js
@@ -6,6 +6,6 @@ function updateVisitCount() {
     fetch('https://c6jga50pg2.execute-api.eu-west-2.amazonaws.com/Prod')
         .then(res => res.json())
         .then(res => {
-        visit_counter.innerHTML = res.website;
+        visit_counter.innerHTML = res.visitors;
     });
 }
