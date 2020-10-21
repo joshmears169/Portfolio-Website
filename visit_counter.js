76ffb1a31ac6232feb600e5f155ceb1c16f135c6
Josh Mears <joshmears169@hotmail.com>
2020-10-21 23:47:33 +0100

    Testing3

--- visit_counter.js
+++ visit_counter.js
@@ -3,7 +3,13 @@ const visit_counter = document.getElementById('count');
 updateVisitCount();
 
 function updateVisitCount() {
-    fetch('https://3pnd9hoy3j.execute-api.eu-west-2.amazonaws.com/Prod/VisitorCounter'
+    fetch('https://3pnd9hoy3j.execute-api.eu-west-2.amazonaws.com/Prod/VisitorCounter', {
+    mode: 'cors',
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET"
+        }
+    }
         )
         .then(res => res.json())
         .then(res => {
