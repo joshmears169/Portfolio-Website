9c54ba3e5da3f2017cba33153f74ed18d625db0d
Josh Mears <joshmears169@hotmail.com>
2020-10-21 16:15:43 +0100

    updated code

--- visit_counter.js
+++ visit_counter.js
@@ -3,9 +3,11 @@ const visit_counter = document.getElementById('count');
 updateVisitCount();
 
 function updateVisitCount() {
-    fetch('https://c6jga50pg2.execute-api.eu-west-2.amazonaws.com/Prod')
+    fetch('https://dhtbtrg610.execute-api.eu-west-2.amazonaws.com/Prod', {
+        mode: 'cors'
+    })
         .then(res => res.json())
         .then(res => {
-        visit_counter.innerHTML = res.visitors;
+        visit_counter.innerHTML = Math.trunc(res.body.visitors);
     });
 }
